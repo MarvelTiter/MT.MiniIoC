55682f462e5606ed600f17c2c08026935e1d32ab
MarvelTiter <outlinmt@outlook.com>
2021-06-19 00:36:58 +0800

    ad  test proj

---
+++ MT.MiniIoc.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MT.MiniIoc.Test {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
